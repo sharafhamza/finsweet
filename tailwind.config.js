3e0acc04bbc3293381f41aa1d57ff40072dc6094
sharafhamza <kingsharaf14@gmail.com>
2023-08-16 16:06:54 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1C1E53',
+        secondary: "#282938",
+        third:"#2405F2",
+        menu: "#BBBBCB",
+        sectioncolor: "#EEF4FA",
+        button: "#FCD980",
+        footercolor:"#F4F6FC",
+        bordercolor: "#27295C",
+        offwhite: "rgba(255, 255, 255, 0.1)",
+        icon: "#97DAAD",
+      },
+      fontFamily: {
+        pop: ["Poppins"],
+        man: ["Manrope"],
+      },
+      fontSize: {
+        large: "54px",
+        xl:"38px",
+        medium: "32px",
+        small: "12px",
+      },
+    },
+  },
+  plugins: [],
+}
+
